8bb3a5f179020e63bfe89042f0619da417984915
GundohNosipho <nosipho@getworth.co.za>
2021-09-05 12:26:33 +0200

    Simple java
    new simple calculator

---
+++ calculator/src/main/java/com/myboston/calculator/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.myboston.calculator;
+
+/**
+ *
+ * @author getWorth
+ */
+public class Main {
+    public static void main(String[] args) {
+        CalculatorEditorUI myClass = new CalculatorEditorUI();
+        myClass.setVisible(true);
+        System.out.println("hello");
+    }
+}
